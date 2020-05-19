3c290786208492d784c5f0d67b36b7c76e27999c
rashmipbhat <bhatrashmip9@gmail.com>
2020-05-19 15:03:57 +0530

    Added a header to the app

---
+++ src/components/WeatherLayout.js
@@ -0,0 +1,13 @@
+import React from "./node_modules/react";
+import { render } from "./node_modules/react-dom";
+
+class weatherLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Weather App</div>;
+  }
+}
+export default weatherLayout;
